8d99d8e10651683456d038cc5794c125f1612f59
melis <melissanoemi.laurent@free.fr>
2023-12-24 23:00:53 +0100

    add business logic for the form add movie seen

--- app/src/main/java/com/cineschoolproject/view/bottomSheet/FormRegisterMovieSeenBottomSheet.kt
+++ app/src/main/java/com/cineschoolproject/view/bottomSheet/FormRegisterMovieSeenBottomSheet.kt
@@ -6,16 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.cineschoolproject.R
+import com.cineschoolproject.exceptions.MovieSeenExceptionMessages
 import com.cineschoolproject.models.movie_model.dto.RegisterMovieSeenRequest
 import com.cineschoolproject.view.BottomSheetListener
 import com.cineschoolproject.viewModel.MovieSeenViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+private const val MIN_THRESHOLD_NOTE = 0
+private const val MAX_THRESHOLD_NOTE = 5
+private const val FORMAT_DATE = "dd-MM-yyyy"
+
 
 class FormRegisterMovieSeenBottomSheet(
     private val bottomSheetListener : BottomSheetListener
 ) : BottomSheetDialogFragment() {
+
     private val movieSeenViewModel : MovieSeenViewModel by viewModel()
     private lateinit var  saveMovieSeenButton: Button
     private lateinit var  dateInputEditText: EditText
@@ -46,19 +56,58 @@ class FormRegisterMovieSeenBottomSheet(
     }
 
     private fun addMovieSeen() {
-        val registerMovieSeenRequest = RegisterMovieSeenRequest(
-            this.dateInputEditText.text.toString(),
-            this.noteInputEditText.text.toString().toInt(),
-            this.commentInputEditText.toString()
-        )
+        val date = dateInputEditText.text.toString()
+        val noteEditText = noteInputEditText.text.toString()
+        val commentEditText = commentInputEditText.text.toString()
 
-        // TODO : pass information from movie detail
-        val title = "Iron man"
-        val imageUrl = "https://image.tmdb.org/t/p/w300/9BBTo63ANSmhC4e6r62OJFuK2GL.jpg"
-        val id = 3
-        this.movieSeenViewModel.addMovieSeen(registerMovieSeenRequest, title, imageUrl, id)
-        bottomSheetListener.onBottomSheetDismissed()
-        dismiss()
+        if (checkForm(date, noteEditText, commentEditText)) {
+            val registerMovieSeenRequest = RegisterMovieSeenRequest(
+                date,
+                noteEditText.toInt(),
+                commentEditText
+            )
+
+            // TODO : passer les informations depuis les détails du film
+            val title = "Iron man"
+            val imageUrl = "https://image.tmdb.org/t/p/w300/9BBTo63ANSmhC4e6r62OJFuK2GL.jpg"
+            val id = 3
+
+            movieSeenViewModel.addMovieSeen(registerMovieSeenRequest, title, imageUrl, id)
+            bottomSheetListener.onBottomSheetDismissed()
+            dismiss()
+        }
+    }
+
+    private fun checkForm(date: String, noteEditText: String, commentEditText: String): Boolean {
+        if (date.isBlank() || noteEditText.isBlank() || commentEditText.isBlank()) {
+            showError(MovieSeenExceptionMessages.EMPTY_FIELDS.message)
+            return false
+        }
+
+        val noteFormattedInt = try {
+            noteEditText.toInt()
+        } catch (e: NumberFormatException) {
+            showError(MovieSeenExceptionMessages.INVALID_NOTE.message)
+            return false
+        }
+
+        if(noteFormattedInt < MIN_THRESHOLD_NOTE || noteFormattedInt > MAX_THRESHOLD_NOTE) {
+            showError(MovieSeenExceptionMessages.INVALID_THRESHOLD_NOTE.message)
+            return false
+        }
+
+        return try {
+            val dateFormat = SimpleDateFormat(FORMAT_DATE, Locale.getDefault())
+            dateFormat.parse(date)
+            true
+        } catch (e: Exception) {
+            showError(MovieSeenExceptionMessages.INVALID_FORMAT_DATE.message)
+            false
+        }
+    }
+
+    private fun showError(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
 }
