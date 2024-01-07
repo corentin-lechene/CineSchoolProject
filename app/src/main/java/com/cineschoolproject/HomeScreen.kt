d49701a8607fa7a1cc8d90b7a5778a787186b400
eduardo.pina <jeseduardopi@gmail.com>
2024-01-07 21:59:31 +0100

    Update home activity with Pager2

---
+++ app/src/main/java/com/cineschoolproject/HomeScreen.kt
@@ -0,0 +1,12 @@
+package com.cineschoolproject.viewModel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cineschoolproject.R
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
